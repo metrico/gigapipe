3012aca93e455d880ac2ee315e520ccbce69b38b
akvlad <akvlad90@gmail.com>
2025-12-22 14:50:11 +0000

    init

--- reader/logql/logql_transpiler/internal/planner/generic.go
+++ reader/logql/logql_transpiler/internal/planner/generic.go
@@ -1,10 +1,13 @@
 package planner
 
 import (
+	"errors"
 	"io"
 
 	"github.com/metrico/qryn/v4/reader/logql/logql_transpiler/shared"
 )
+
+var errSkipEntry = errors.New("skip entry")
 
 type GenericPlanner struct {
 	Main shared.RequestProcessor
@@ -32,13 +35,40 @@ func (g *GenericPlanner) WrapProcess(ctx *shared.PlannerContext,
 		defer close(out)
 		defer func() { shared.TamePanic(out) }()
 		for entries := range _in {
+			skipMask := make([]uint64, (len(entries)+63)/64)
 			for i := range entries {
 				err := ops.OnEntry(&entries[i])
-				if err != nil && err != io.EOF {
-					onErr(err)
-					return
+				if err == nil || errors.Is(err, io.EOF) {
+					continue
 				}
+				if errors.Is(err, errSkipEntry) {
+					skipMask[i/64] |= 1 << (i % 64)
+					continue
+				}
+				onErr(err)
+				return
 			}
+
+			shift := 0
+			for i, m := range skipMask {
+				var start, end int
+				for m != 0 {
+					for m&1 == 0 {
+						m >>= 1
+						start++
+						end++
+					}
+					for m&1 != 0 {
+						m >>= 1
+						end++
+					}
+					copy(entries[i*64-shift+start:], entries[i*64-shift+end:])
+					entries = entries[:len(entries)-end+start]
+					shift += end - start
+				}
+
+			}
+
 			err := ops.OnAfterEntriesSlice(entries, out)
 			if err != nil && err != io.EOF {
 				onErr(err)
