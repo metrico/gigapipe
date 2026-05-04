549a17d498aba8b6a4f46b8534ad1113603a9dcd
rita7lopes <105070795+rita7lopes@users.noreply.github.com>
2026-05-04 15:05:59 +0100

    Fix/headers (#795)
    * fix headers order to avoid browsers could content-sniff the response as HTML and
    
    * sort code scan alerts
    
    * html snitizer to avoid breaking changes for the error message in pyroscope

--- reader/controller/prof.go
+++ reader/controller/prof.go
@@ -355,5 +355,5 @@ func defaultMarshaller[T proto.Message](r *http.Request, t T) ([]byte, error) {
 func defaultError(w http.ResponseWriter, code int, message string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	w.Write([]byte(strconv.Quote(message)))
+	w.Write([]byte(strconv.Quote(html.EscapeString(message))))
 }
